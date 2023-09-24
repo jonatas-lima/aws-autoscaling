a48037008802fbe75fc08e755d94b35facca6cf4
Jonatas Ferreira <62180037+jonatas-lima@users.noreply.github.com>
2023-09-24 11:06:38 -0300

    Update app/src/main/java/com/example/arquitera_software/TestController.java

--- app/src/main/java/com/example/arquitera_software/TestController.java
+++ app/src/main/java/com/example/arquitera_software/TestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public ResponseEntity<Void> testEndpoint() {
         System.out.println("START :: /test ENDPOINT");
         try {
