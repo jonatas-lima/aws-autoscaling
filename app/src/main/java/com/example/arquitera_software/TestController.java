203de09ace93f0fc37741e894c5513c31ef9c1fc
hiarlyfs <hiarly.souto@cyan-agro.com>
2023-10-02 18:31:36 -0300

    Fix App to make Math calculation inside threads

--- app/src/main/java/com/example/arquitera_software/TestController.java
+++ app/src/main/java/com/example/arquitera_software/TestController.java
@@ -7,22 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class TestController {
 
+    private static final int NUMBER_OF_THREADS = 1000;
+    private static final int NUMBER_OF_MULTIPLICATIONS = 5000;
     @GetMapping("/")
     public ResponseEntity<Void> testEndpoint() {
         System.out.println("START :: /test ENDPOINT");
         try {
             List<Thread> threads = new ArrayList<>();
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < NUMBER_OF_THREADS; i++) {
                 Thread t = new Thread(() -> {
-                    try {
-                        Thread.sleep(10000);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        throw new RuntimeException(e);
+                    for (int j = 0; j < NUMBER_OF_MULTIPLICATIONS; j ++) {
+                        Random random = new Random();
+                        double result = random.nextDouble() * random.nextDouble();
+                        System.out.println("result :: " + result);
                     }
                 });
 
